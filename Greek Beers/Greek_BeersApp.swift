261c6568d0d63ffd15d71e02c8bcb4c7b16168b3
George <giorgio_sid@yahoo.gr>
2023-07-11 17:50:34 +0300

    Initial Commit

---
+++ Greek Beers/Greek_BeersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Greek_BeersApp.swift
+//  Greek Beers
+//
+//  Created by George Sidiropoulos on 11/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Greek_BeersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
